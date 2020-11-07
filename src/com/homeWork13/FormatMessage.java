051868c373d5449c076ca6fc345d7ce250ffd5f0
Alex Kalmus <kalmus2111@gmail.com>
2020-11-07 12:00:31 +0200

    Home Work Completed! New version

--- src/com/homeWork13/FormatMessage.java
+++ src/com/homeWork13/FormatMessage.java
@@ -5,33 +5,31 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class FormatMessage implements Logger{
-    private Date date = new Date();
     private DateFormat dateF = new SimpleDateFormat("dd.MM.yy HH:mm:ss");
 
 
     @Override
-    public void info(String message) {
+    public void info(String message)
+    {
+        Date date = new Date();
         System.out.println("INFO: "+dateF.format(date)+" : "+message);
     }
 
     @Override
     public void info(String message, Customer customer) {
+        Date date = new Date();
         System.out.println("INFO: "+customer.toString()+dateF.format(date)+" : "+message);
     }
 
     @Override
     public void error(String message) {
+        Date date = new Date();
         System.err.println("ERROR: "+dateF.format(date)+" : "+message);
     }
 
     @Override
     public void error(String message, Customer customer) {
+        Date date = new Date();
         System.err.println("ERROR: "+customer.toString()+dateF.format(date)+" : "+message);
     }
-
-//    public void infoByLog (Customer customer, String message){
-//        if (!customer.isLogined())
-//            info(message);
-//        else info(message, customer);
-//    }
 }
